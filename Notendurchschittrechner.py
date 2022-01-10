7f3b5186aee55bf5716498c45a6112b1244a3877
loics123 <97448892+loics123@users.noreply.github.com>
2022-01-10 14:03:07 +0100

    Update Notendurchschittrechner.py

--- Notendurchschittrechner.py
+++ Notendurchschittrechner.py
@@ -1,15 +1,19 @@
+# Dieses Programm kann den Notendurchschnitt berechen. Man kann die Noten hinzufügen und dann wird der aktuelle Notendurchschnitt berechnet. So weiss man welchen Notendurchschnitt man im Moment hat.
+
 note = []
 
-while True:     # : nicht vergessen!
+while True:     # Fügen sie ihre Noten hinzu
     Note = float(input("Welche Note wollen sie hinzufügen? "))
     note.append(Note)
 
-    #Notendurchschnitt ausrechnen
+    # Dies berechnet den Durchschnitt von den hinzu gefügten Noten
     summe = 0
     n = len(note)
     for Note in note: 
         summe = summe + Note
     Notendurchschnitt = summe / n
 
+    # Der Durchschnitt der Noten wird in der Ausgabe ausgegeben
+    
     print("Noten: " + str(note))
     print("Durchschnitt: " + str(Notendurchschnitt))
