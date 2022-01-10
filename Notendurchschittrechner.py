019829c622a73b38823f158b2c249698d5b392b5
loics123 <97448892+loics123@users.noreply.github.com>
2022-01-10 14:20:21 +0100

    Update Notendurchschittrechner.py

--- Notendurchschittrechner.py
+++ Notendurchschittrechner.py
@@ -1,11 +1,11 @@
 
 note = []
 
-while True:     # Fügen sie ihre Noten hinzu
-    Note = float(input("Welche Note wollen sie hinzufügen? "))
+while True:
+    Note = float(input("Welche Note wollen sie hinzufügen? ")) # Hier wird das Programm sie fragen, welche Note sie hinzufügen wollen
     note.append(Note)
 
-    # Dies berechnet den Durchschnitt von den hinzu gefügten Noten
+    # Dies berechnet den Durchschnitt von den hinzugefügten Noten
     summe = 0
     n = len(note)
     for Note in note: 
@@ -13,6 +13,5 @@ while True:     # Fügen sie ihre Noten hinzu
     Notendurchschnitt = summe / n
 
     # Der Durchschnitt der Noten wird in der Ausgabe ausgegeben
-    
     print("Noten: " + str(note))
     print("Durchschnitt: " + str(Notendurchschnitt))
